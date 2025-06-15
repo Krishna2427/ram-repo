fbf4cda66d8b1bbf3cd2f44a1b00392c380e4a03
Krishna2427 <krishnalankapally8@gmail.com>
2025-06-15 18:16:20 +0530

    Added project files

---
+++ krishna_code/src/App.js
@@ -0,0 +1,15 @@
+
+// src/App.js
+import React from 'react';
+import About from './About';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from test01 branch</h1>
+      <About />
+    </div>
+  );
+}
+
+export default App;
