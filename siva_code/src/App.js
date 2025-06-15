14dd6a6b4a9b5638bb46bbdd10ae31bfab9855f8
Krishna2427 <krishnalankapally8@gmail.com>
2025-06-15 18:25:06 +0530

    Added project files

---
+++ siva_code/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';;
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from test02 branch</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
